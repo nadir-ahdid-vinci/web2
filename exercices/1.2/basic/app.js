3ad4442ba3ced237bbbc7528e22a2259a0de1d13
Nadir Ahdid <nadir.ahdid@student.vinci.be>
2023-09-19 10:32:51 +0200

    ff

--- exercices/1.2/basic/app.js
+++ exercices/1.2/basic/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-let counter = [0,0,0,0];
+let counter = [{path : req.path + req.method},{counter : 0}];
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,24 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    if(req.method === "GET" && req.path === "/"){
-        counter[0]++;
-    }
-    if(req.method === "GET" && req.path === "/pizzas"){
-        counter[1]++;
-    }
-    if(req.method === "POST" && req.path === "/pizzas"){
-        counter[2]++;
-    }
-    if(req.method === "DELETE" && req.path === "/pizzas"){
-        counter[3]++;
-    }
-
-    console.log("Request counter : ");
-    console.log("- GET / : " + counter[0]);
-    console.log("- GET /pizzas : " + counter[1]);
-    console.log("- POST /pizzas : " + counter[2]);
-    console.log("- DELETE /pizzas : " + counter[3]);
+    
     next();
   });
   
