fdd2e5e17cd6739bbc9a7763f03a256e2892aac5
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-09-24 23:26:10 +0200

    rr

--- exercices/1.2/basic/app.js
+++ exercices/1.2/basic/app.js
@@ -12,11 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-    
-    next();
-  });
   
 
 app.use('/', indexRouter);
