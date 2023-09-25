4eaec047520659fe3560a8653b19994e397f3da5
Nadir Ahdid <nadir.ahdid@student.vinci.be>
2023-09-25 13:58:11 +0200

    ff

---
+++ exercices/1.3/basic/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+let counter = [{path : req.path + req.method},{counter : 0}];
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+  
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
